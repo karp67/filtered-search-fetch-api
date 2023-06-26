73fd08e16d4b83fb47045818a544cd527bb863e2
Poulomi1234 <kar.poulomi2896@gmail.com>
2023-06-26 21:07:04 +0530

    rendering dynamic filtered list from an api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [inputText, setInputText] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    const fetchdata = async () => {
+      const response = await fetch("https://randomuser.me/api/?results=100");
+      const jsonData = await response.json();
+      setData(jsonData.results);
+      setFilteredData(jsonData.results);
+    };
+
+    fetchdata();
+  }, []);
+
+  const filterDataHandler = (event) => {
+    setInputText(event.target.value);
+    filterData(event.target.value);
+  };
+
+  const filterData = (text) => {
+    const filtered = data.filter((user) =>
+      user.name.first.toLowerCase().includes(text.toLowerCase())
+    );
+    setFilteredData(filtered);
+  };
+
+  const resetHandler = () => {
+    setInputText("");
+    setFilteredData([]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <input
+        value={inputText}
+        type="text"
+        placeholder="Search..."
+        className="search"
+        onChange={filterDataHandler}
+      ></input>
+      <button onClick={resetHandler}>Reset</button>
+      <ul className="list">
+        {filteredData.map((user) => (
+          <li className="listItem">{user.name.first}</li>
+        ))}
+      </ul>
     </div>
   );
 }
